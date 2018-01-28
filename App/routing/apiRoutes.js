a8bf71f8d27dc67352191f637209a51814168b2d
Matt Quade <quadem09@gmail.com>
2018-01-28 22:03:05 +0000

    post not pushing scores correct

--- App/routing/apiRoutes.js
+++ App/routing/apiRoutes.js
@@ -5,15 +5,22 @@ var friendData = require("../data/friends");
 //Routing
 module.exports = function(app) {
 
-
+    //GETs all the friendData
     app.get("/api/friends", function(req, res) {
         res.json(friendData);
     });
 
-
+    //POST to make a new friendData
     app.post("/api/friends", function(req, res) {
         friendData.push(req.body);
         res.json(true);
+    });
+
+    //GETs all the friendData scores
+    app.get("/api/friends/scores", function(req, res) {
+        for (var i = 0; i < friendData.length; i++) {
+            res.json(friendData[i].scores);
+        }
     });
 
 };
--- PSEUDO_unfinished.md
+++ PSEUDO_unfinished.md
@@ -3,8 +3,9 @@ Things I was able to accomplish:
     I can record user input from survey
     I can pass something to friends.js
     created routes for all the pages
+    POST JSON data to for newFriends.js
     
 Things to complete
-    push JSON data to friends.js
+   
     compare scores
     And use a Modal?
